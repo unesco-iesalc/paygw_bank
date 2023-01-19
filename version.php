07b925b44c48ce952852cf7018f0d5deb16baea1
cvicente <vicencarlos@gmail.com>
2023-01-19 20:23:32 +0100

    Space

--- version.php
+++ version.php
@@ -23,6 +23,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+ 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023011901;        // The current plugin version (Date: YYYYMMDDXX).
